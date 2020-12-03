eba6d17a95d8530e9447df0ce1ac37db29f1ffce
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 23:54:01 +0300

    2020:12:03 23:54

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='getdirs',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
